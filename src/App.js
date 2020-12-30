b36f1064af4f55264b7be03fd2cdd2a4835eacf6
OlyaPinchuk <olha5959@gmail.com>
2020-12-30 18:04:42 +0200

    Add a part of hw8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import React, {Component} from 'react';
+import React, {memo, useState, useEffect, useMemo, useCallback} from 'react';
 
 
-class App extends Component {
+const Test = memo (({onClick}) => {
+
+    console.log('this is test')
+
+    return (
+
+        <div onClick = {onClick} > test here </div>
+
+    )
+
+})
 
 
 
+export default function App() {
+
+    const [counter, setCounter] = useState(0)
+
+    const handleInc = () => {
+        setCounter((prevState) => prevState + 1)
+
+    }
+
+    const handleDec = () => {
+        setCounter((prevState) => prevState - 1)
+
+    }
+
+    const handleReset = () => {
+        setCounter(0)
+
+    }
+
+    const onClickHandler = useCallback(() => console.log(counter) , [counter])
+
+    return (
+        <div>
+            <Test onClick = {onClickHandler} />
+            <h2> Counter: {counter} </h2>
+            <button onClick = {handleInc} > increase </button>
+            <button onClick = {handleDec} > decrease </button>
+            <button onClick = {handleReset} > reset </button>
+
+
+        </div>
+
+    )
 
 }
-export default App;
