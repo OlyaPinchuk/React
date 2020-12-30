b79a3046e497db3089f602e925bbd60db8e1673e
irynadobr <irenkad@ukr.net>
2020-12-30 21:24:54 +0200

    Commit from Okten

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
-import React, {} from 'react';
+import React, {useState, useEffect} from 'react';
+import User from './components/User'
+
+
+
+// const InputWindow = () => {
+//     return (
+//         <div>
+//             {/*<User item = {'item here'} />*/}
+//
+//         </div>
+//     )
+// }
+
+
+const handleSave = () => {
+
+    // setState((prevState) => {})
+    console.log('works')
+
+}
+
 
 
 export default function App() {
 
+    const [state, setState] = useState(null)
 
+    return (
+
+        <div>
+
+            <form>
+
+                <div>User name</div>
+                <input></input>
+                <div>User phone</div>
+                <input></input>
+
+                <button type = 'submit' onClick={handleSave} > save </button>
+
+            </form>
+        </div>
+
+    )
 
 };
---
+++ src/components/User.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+export default function App({item}) {
+
+
+
+    return (
+
+        <div>
+            {item}
+
+        </div>
+
+    )
+
+};
