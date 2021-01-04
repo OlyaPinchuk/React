15b5c623bd463a18564c275e685a561e952fe9ec
OlyaPinchuk <olha5959@gmail.com>
2021-01-04 19:46:26 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
+//https://codesandbox.io/s/distracted-mendeleev-hi0k6?file=/src/services/index.js:126-140
 import React, {useState, useEffect, useRef} from 'react';
-import User from './components/User'
 
-
-
-// const InputWindow = () => {
-//     return (
-//         <div>
-//             {/*<User item = {'item here'} />*/}
-//
-//         </div>
-//     )
-// }
-
-
-//const handleSave = (ev) => {
-//
-//    ev.preventDefault()
-//    // setState((prevState) => {})
-//    console.log('works')
-//    console.log(textInput.value)
-//
-//
-//}
 
 
 
 export default function App() {
-
-    const [state, setState] = useState(null)
-    const nameInput = useRef(null)
-
-
-    const handleSave = (ev) => {
-
-//        ev.preventDefault()
-        setState({name: nameInput.current.value, age: 20})
-        nameInput.current.value = null
-//        console.log(nameInput.current.value)
-//        console.log(state)
-    }
-
-    console.log(state)
-
-
 
 
 
@@ -50,21 +12,6 @@ export default function App() {
 
         <div>
 
-            <div>
-
-                <div>User name</div>
-                <input ref = {nameInput} ></input>
-
-                <div>User age</div>
-                <input></input>
-
-                <button type = 'submit' onClick={handleSave} > save </button>
-
-
-                <hr/>
-                <User item = {state} />
-
-            </div>
 
 
         </div>
