135f9133cb7b42dde224c39d9d1bd08b8f8a00f3
OlyaPinchuk <olha5959@gmail.com>
2021-01-06 04:39:09 +0200

    Add a part of hw.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 //https://codesandbox.io/s/distracted-mendeleev-hi0k6?file=/src/services/index.js:126-140
 import React, {useState, useEffect, useRef} from 'react';
-
+import {useSelector} from 'react-redux'
 
 
 
 export default function App() {
 
+    const {cart, wishlist, products} = useSelector(
+       ({ cart: { cart }, wishlist: { wishlist }, products: { products } }) => ({
+          cart,
+          wishlist,
+          products
+        })
+    )
 
 
     return (
 
         <div>
 
-
+            {cart.length} {wishlist.length} {products.length}
 
         </div>
 
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+import {combineReducers} from 'redux';
+import cart from './cart'
+import products from './products'
+import wishlist from './wishlist'
+
+export const reducer = combineReducers({
+    cart,
+    wishlist,
+    products,
+
+})
