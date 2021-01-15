02567b67d5cb4de156efc7312f5e6e4e3ca25235
OlyaPinchuk <olha5959@gmail.com>
2021-01-15 03:46:29 +0200

    Add changes.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,13 +4,15 @@ import logo from '../../logo.png'
 import catalogue from '../../catalogue.png'
 import search from '../../search.png'
 import mic from '../../mic.png'
-import {Navbar, Nav, Button, NavDropdown, Form, FormControl, Row, Col, Container} from 'react-bootstrap'
+import {Image, ListGroup, ListGroupItem, Navbar, Nav, Button, NavDropdown, Form, FormControl, Row, Col, Container} from 'react-bootstrap'
 import {useSelector} from "react-redux";
 
 
 
 
 export default function Header() {
+
+    const [state, setState] =useState({searchText: ''})
 
     const {products, cart, wishlist} = useSelector(
        ({ products: { products }, cart: { cart }, wishlist: { wishlist } }) => ({
@@ -31,11 +33,11 @@ export default function Header() {
     return(
         <div>
 
-            <nav id = 'addBar' className="navbar-add d-none d-lg-block">Реклама тут</nav>
-            <nav id='topNavbar' className=" container-fluid navbar navbar-expand-lg text-white d-none d-lg-block">
+            <Nav id = 'addBar' className="navbar-add d-none d-lg-block">Реклама тут</Nav>
+            <Nav id='topNavbar' className=" container-fluid navbar navbar-expand-lg text-white d-none d-lg-block">
 
                 <div className=" container-fluid collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul className=" row navbar-nav mr-auto mt-2 mt-md-0">
+                    <ListGroup className=" row navbar-nav mr-auto mt-2 mt-md-0">
                         <li className=" col-sm-2 nav-item">
                             <a className=" top-nav-bar-btn nav-link text-white" href="#!">44 444</a>
                         </li>
@@ -64,47 +66,53 @@ export default function Header() {
                             <a className=" top-nav-bar-btn nav-link text-white" href="#!">аккаунт</a>
                         </li>
 
-                    </ul>
+                    </ListGroup>
                 </div>
-            </nav>
+            </Nav>
 
 
-            <nav className=" container-fluid navbar navbar-expand-lg navbar-light">
+            <Nav className=" container-fluid navbar navbar-expand-lg navbar-light">
                 <div className='row'>
                     <a  id='logo' className="navbar-brand " href="#">
-                        <img src={logo} alt="Rozetka logo" width="270" height="40px"/>
+                        <Image src={logo} alt="Rozetka logo" width="270" height="40px"/>
                     </a>
 
                     <div className="main-navbar-item collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
                                 <a id = 'catalogue' className="nav-link text-white " href="#">
-                                    <img id = 'catalogue-pic' src={catalogue} alt='Catalogue pic'/>
+                                    <Image id = 'catalogue-pic' src={catalogue} alt='Catalogue pic'/>
                                       Каталог товарів
                                 </a>
                             </li>
                         </ul>
 
-                        <form id = 'form' className="main-navbar-item form-inline d-block">
+
+                        {/*<Form id = 'form' className="main-navbar-item form-inline d-block">*/}
 
                             <div className="input-group">
                                 <div className="input-group-prepend">
                                     <span id = 'search' className="btn">
-                                        <img src={search} />
+                                        <Image src={search} />
                                     </span>
                                 </div>
-                                <input id='search-input' type="text" className="form-control" placeholder="Я шукаю..."
-                                       aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                                <input id='search-input'
+                                       type="text"
+                                       // value={state.searchText}
+                                       // className="form-control"
+                                       placeholder="Я шукаю..."
+                                       onChange={console.log('works')}
+                                       />
 
                                 <div className="input-group-append">
-                                    <button id = 'mic' className="btn" type="button">
-                                        <img id = 'mic-pic' src={mic} />
-                                    </button>
-                                    <button id='find' className="btn bg-success text-white my-2 my-sm-0" type="submit">Знайти</button>
+                                    <Button id = 'mic' className="btn" type="button">
+                                        <Image id = 'mic-pic' src={mic} />
+                                    </Button>
+                                    <Button id='find' className="btn bg-success text-white my-2 my-sm-0" type="submit">Знайти</Button>
                                 </div>
                             </div>
 
-                        </form>
+                        {/*</Form>*/}
 
                         <div className = ' main-navbar-item icon btn btn-sm text-white d-none d-lg-block'>
                             <div>Спробуйте</div>
@@ -116,7 +124,7 @@ export default function Header() {
 
                     </div>
                 </div>
-            </nav>
+            </Nav>
 
 
 
