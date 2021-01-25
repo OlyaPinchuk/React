09e568e1786d0f19c8f58066cf8ffeea3ff4d08b
OlyaPinchuk <olha5959@gmail.com>
2021-01-25 02:18:27 +0200

    Add changes

--- src/pages/movieDetails/MovieDetails.js
+++ src/pages/movieDetails/MovieDetails.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import {useRouteMatch} from 'react-router-dom'
+import React, {useEffect, useState} from 'react';
+import {useRouteMatch, useParams} from 'react-router-dom'
+import {movieService} from "../../services";
+import {FilmItem} from "../../components";
 
-export const MovieDetails = ({children}) => {
-    const {params: {id}} = useRouteMatch()
+const imgBuilder = (posterPath, size = 200) => `http://image.tmdb.org/t/p/w${size}${posterPath}`
+
+
+export const MovieDetails = ({}) => {
+
+    const [movieDetails, setMovieDetails] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+    const {id, ...rest} = useParams()
+
+    const getMovieDetails = async() => {
+        try {
+            setIsLoading(true)
+            const data = await movieService.getMovieById(id)
+            setMovieDetails(data)
+        } catch (e) {
+            console.log(e)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getMovieDetails()
+    }, [])
+
+
+    if (isLoading && !movieDetails || isLoading === null) {
+        console.log('loading')
+        return (
+            <h3>Loading...</h3>
+        )
+    }
 
     return (
         <div>
-           hello
+            {movieDetails &&
+
+                <div style ={{
+                    height: 300,
+                    width: "100%",
+                    backgroundImage: `url(${imgBuilder(movieDetails.poster_path)})`,
+                    backgroundRepeat: 'no-repeat',
+                }}>
+                </div>
+            }
+
+           <h1>{movieDetails && movieDetails.original_title}</h1>
+           <h3>{movieDetails && movieDetails.genres.map((el, i) => <span key={el.id}>{el.name} {i < movieDetails.genres.length -1 && '-'} </span> )}</h3>
+           <p>{movieDetails && movieDetails.overview}</p>
         </div>
     )
 }
